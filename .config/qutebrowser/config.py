a56cb592652d09be59bb7525845ff3aaa11d98c8
turbo <yicunzhu84@gmail.com>
2018-04-22 19:59:50 +0800

    add qutebrowser

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,99 @@
+# Encoding to use for the editor.
+c.url.searchengines = {'DEFAULT': 'https://www.bing.com/search?q={}'}
+c.url.default_page = 'https://legacy.gitbook.com/@yeasy' 
+c.content.headers.user_agent = 'Chrome/19.0.1084.52'
+# Type: Encoding
+c.editor.encoding = 'utf-8'
+
+# Font used in the completion categories.
+# Type: Font
+c.fonts.completion.category = 'bold 10pt monospace'
+
+# Font used in the completion widget.
+# Type: Font
+c.fonts.completion.entry = '10pt monospace'
+
+# Font used for the debugging console.
+# Type: QtFont
+c.fonts.debug_console = '10pt monospace'
+
+# Font used for the downloadbar.
+# Type: Font
+c.fonts.downloads = '10pt monospace'
+
+# Font used for the hints.
+# Type: Font
+c.fonts.hints = 'bold 12pt monospace'
+
+# Font used in the keyhint widget.
+# Type: Font
+c.fonts.keyhint = '10pt monospace'
+
+# Font used for error messages.
+# Type: Font
+c.fonts.messages.error = '10pt monospace'
+
+# Font used for info messages.
+# Type: Font
+c.fonts.messages.info = '10pt monospace'
+
+# Font used for warning messages.
+# Type: Font
+c.fonts.messages.warning = '10pt WenQuanYi Micro Hei Mono'
+
+# Default monospace fonts. Whenever "monospace" is used in a font
+# setting, it's replaced with the fonts listed here.
+# Type: Font
+c.fonts.monospace = '"WenQuanYi Micro Hei Mono", "Monofur for Powerline", Monospace, "DejaVu Sans Mono", Monaco, "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", Courier, "Liberation Mono", monospace, Fixed, Consolas, Terminal'
+
+# Font used for prompts.
+# Type: Font
+c.fonts.prompts = '10pt WenQuanYi Micro Hei Mono'
+
+# Font used in the statusbar.
+# Type: Font
+c.fonts.statusbar = '10pt WenQuanYi Micro Hei Mono'
+
+# Font used in the tab bar.
+# Type: QtFont
+c.fonts.tabs = '10pt WenQuanYi Micro Hei Mono'
+
+# Font family for cursive fonts.
+# Type: FontFamily
+c.fonts.web.family.cursive = 'WenQuanYi Micro Hei Mono'
+
+# Font family for fantasy fonts.
+# Type: FontFamily
+c.fonts.web.family.fantasy = 'WenQuanYi Micro Hei Mono'
+
+# Font family for fixed fonts.
+# Type: FontFamily
+c.fonts.web.family.fixed = 'WenQuanYi Micro Hei Mono'
+
+# Font family for sans-serif fonts.
+# Type: FontFamily
+c.fonts.web.family.sans_serif = 'WenQuanYi Micro Hei Mono'
+
+# Font family for serif fonts.
+# Type: FontFamily
+c.fonts.web.family.serif = 'WenQuanYi Micro Hei Mono'
+
+# Font family for standard fonts.
+# Type: FontFamily
+c.fonts.web.family.standard = 'WenQuanYi Micro Hei Mono'
+
+# The default font size for regular text.
+# Type: Int
+c.fonts.web.size.default = 16
+
+# The default font size for fixed-pitch text.
+# Type: Int
+c.fonts.web.size.default_fixed = 13
+
+# The hard minimum font size.
+# Type: Int
+c.fonts.web.size.minimum = 0
+
+# The minimum logical font size that is applied when zooming out.
+# Type: Int
+c.fonts.web.size.minimum_logical = 6
